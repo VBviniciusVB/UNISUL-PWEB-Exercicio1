c105402ab77ef1fcd429a7e5665817f7e73ed24e
VBViniciusVB <viniciusvianalaguna@hotmail.com>
2019-05-29 21:28:42 -0300

    first commit2

---
+++ app/app.module.ts
@@ -0,0 +1,52 @@
+import { ProdutosModule } from './produtos/produtos.module';
+import { ProdutosCadastroComponent } from './produtos/produtos-cadastro/produtos-cadastro.component';
+import { CidadesModule } from './cidades/cidades.module';
+import { CidadesCadastroComponent } from './cidades/cidades-cadastro/cidades-cadastro.component';
+import { ButtonModule } from 'primeng/button';
+import { CategoriasCadastroComponent } from './categorias/categorias-cadastro/categorias-cadastro.component';
+import { CategoriasPesquisaComponent } from './categorias/categorias-pesquisa/categorias-pesquisa.component';
+import { CategoriasModule } from './categorias/categorias.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ConfirmationService } from 'primeng/api';
+import {SidebarModule} from 'primeng/sidebar';
+
+import { CurrencyMaskModule } from "ng2-currency-mask";
+
+import {Routes, RouterModule} from '@angular/router';
+
+const rotas: Routes = [
+  {path: '', redirectTo:'categorias', pathMatch:'full'},
+  {path: 'cidades', component: CidadesCadastroComponent},
+  {path: 'produtos', component:ProdutosCadastroComponent},
+  {path: 'categorias', component: CategoriasPesquisaComponent},
+  {path: 'categorias/novo', component: CategoriasCadastroComponent},
+  {path: 'categorias/:id', component: CategoriasCadastroComponent}
+];
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CategoriasModule,
+    CidadesModule,
+    ProdutosModule,
+    HttpClientModule,
+    SidebarModule,
+    ButtonModule,
+    CurrencyMaskModule,
+    RouterModule.forRoot(rotas)
+  ],
+  providers: [
+    ConfirmationService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ app/categorias/categorias.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CategoriasPesquisaComponent } from './categorias-pesquisa/categorias-pesquisa.component';
+
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+import {TooltipModule} from 'primeng/tooltip';
+import {TableModule} from 'primeng/table';
+import { CategoriasService } from './categorias.service';
+import {ToastModule} from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { FormsModule } from '@angular/forms';
+import {ConfirmDialogModule} from 'primeng/confirmdialog';
+import { CategoriasCadastroComponent } from './categorias-cadastro/categorias-cadastro.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [CategoriasPesquisaComponent, CategoriasCadastroComponent],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    TooltipModule,
+    TableModule,
+    ToastModule,
+    FormsModule,
+    ConfirmDialogModule,
+    RouterModule
+  ],
+  exports:[
+    CategoriasPesquisaComponent,
+    CategoriasCadastroComponent
+  ],
+  providers: [
+    CategoriasService,
+    MessageService
+  ]
+})
+export class CategoriasModule { }
